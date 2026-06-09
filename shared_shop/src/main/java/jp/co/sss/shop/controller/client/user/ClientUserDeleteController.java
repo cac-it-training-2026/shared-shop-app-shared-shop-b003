c44ebea64b844e049bfa1e7bef34eafeb2c8b33f
narumina <narumina@C010422720.zipangu.cac.co.jp>
2026-06-09 09:49:30 +0900

    fix:遷移先修正

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserDeleteController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserDeleteController.java
@@ -4,11 +4,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import jakarta.servlet.http.HttpSession;
+import jp.co.sss.shop.bean.UserBean;
 import jp.co.sss.shop.entity.User;
 import jp.co.sss.shop.form.UserForm;
 import jp.co.sss.shop.repository.UserRepository;
@@ -36,11 +36,12 @@ public class ClientUserDeleteController {
 	 * @return "delete_check" 削除確認画面 表示
 	 *
 	 */
-	@RequestMapping(path = "/client/user/delete/check/{id}", method = RequestMethod.POST)
-	public String deletecheck(@PathVariable Integer id) {
+	@RequestMapping(path = "/client/user/delete/check")
+	public String userDeleteCheck() {
 
-		//		削除対象の会員情報を取得
-		User user = repository.findByIdAndDeleteFlag(id, Constant.NOT_DELETED);
+		//		削除対象の会員情報をセッションスコープから取得
+		UserBean loginUser = (UserBean) session.getAttribute("user");
+		User user = repository.findByIdAndDeleteFlag(loginUser.getId(), Constant.NOT_DELETED);
 
 		if (user == null) {//対象がない場合
 			return "redirect:/syserror";
@@ -70,7 +71,7 @@ public class ClientUserDeleteController {
 		model.addAttribute("userForm", userForm);
 
 		// 削除確認画面　表示
-		return "admin/user/delete_check";
+		return "client/user/delete_check";
 	}
 
 	/**
@@ -105,7 +106,7 @@ public class ClientUserDeleteController {
 		session.removeAttribute("userForm");
 
 		// 削除完了画面　表示処理
-		return "redirect:/client/user/delete/complete";
+		return "redirect:/delete/complete";
 	}
 
 	/**
@@ -113,10 +114,10 @@ public class ClientUserDeleteController {
 	 *
 	 * @return "admin/user/delete_complete" 会員情報 削除完了画面へ
 	 */
-	@RequestMapping(path = "/client/user/delete/complete", method = RequestMethod.GET)
+	@RequestMapping(path = "/delete/complete", method = RequestMethod.GET)
 	public String deleteCompleteFinish() {
 
-		return "client/user/delete_complete";
+		return "/delete_complete";
 	}
 
 }
