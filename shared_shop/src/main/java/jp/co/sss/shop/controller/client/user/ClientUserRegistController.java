be25509c632226d7747ae36c6e5d215af006c803
kamagata <kamagata@C010423140.zipangu.cac.co.jp>
2026-06-10 12:23:28 +0900

    add:登録処理機能の追加、完了画面表示

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserRegistController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/user/ClientUserRegistController.java
@@ -1,11 +1,13 @@
 package jp.co.sss.shop.controller.client.user;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
@@ -29,75 +31,134 @@ public class ClientUserRegistController {
 	@Autowired
 	HttpSession session;
 
-	//①新規会員登録リンクをクリック
+	/**
+	 * ① 新規会員登録リンククリック時
+	 * 
+	 * @return
+	 */
+
 	@RequestMapping(path = "/input/init", method = RequestMethod.GET)
 	public String init() {
-		//空のフォームを作成
-		UserForm form = new UserForm();
-		//セッションに保存
-		session.setAttribute("userForm", form);
-		//入力画面へリダイレクト
+		// 入力画面へリダイレクト
 		return "redirect:/client/user/regist/input";
 	}
 
-	//入力画面の表示
+	/**
+	 * ③入力画面の表示
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(path = "/input", method = RequestMethod.GET)
 	public String registInput(Model model) {
-		UserForm form = (UserForm) session.getAttribute("userForm");
 
-		if (form == null) {
-			form = new UserForm();
+		// 上書きの防止
+		if (!model.containsAttribute("userForm")) {
+
+			// セッションからフォームの取得
+			UserForm form = (UserForm) session.getAttribute("userForm");
+
+			// なかった場合新規作成
+			if (form == null) {
+				form = new UserForm();
+			}
+			// 画面に渡す
+			model.addAttribute("userForm", form);
 		}
-		//正常時にセッション保存
-		model.addAttribute("userForm", form);
+		// 入力画面表示
 		return "client/user/regist_input";
 	}
 
 	/**
-	 * ③ 確認ボタン押下（入力チェック）
+	 *  ②新規登録ボタン押す
+	 */
+	@RequestMapping(path = "/input", method = RequestMethod.POST)
+	public String registSubmit(UserForm form) {
+		// 入力値をセッションに保持
+		session.setAttribute("userForm", form);
+		// 入力画面へリダイレクト
+		return "redirect:/client/user/regist/input";
+	}
+
+	/**
+	 * ④ 確認ボタン押す（入力チェック）
 	 */
 
 	@RequestMapping(path = "/check", method = RequestMethod.POST)
-	public String registCheck(@Valid UserForm form, BindingResult result, Model model) {
+	public String registCheck(@Valid UserForm form, BindingResult result, RedirectAttributes redirectAttributes) {
 		// 入力チェック
 		if (result.hasErrors()) {
-			model.addAttribute("userForm", form);
-			return "client/user/regist_input";
+			// エラー情報をリダイレクト語も使えるように
+			redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userForm", result);
+			// 入力値の保持
+			redirectAttributes.addFlashAttribute("userForm", form);
+			// 入力画面へ戻る
+			return "redirect:/client/user/regist/input";
+
 		}
 
 		// メール重複チェック
 		User user = userRepository.findByEmail(form.getEmail());
 		if (user != null) {
-			result.rejectValue("email", "", "メールアドレスが既に登録されています");
-			model.addAttribute("userForm", form);
-			return "client/user/regist_input";
+
+			// メールが重複した際のエラーメッセージを追加
+			result.rejectValue("email", "error.email.duplicate", "メールアドレスが既に登録されています");//error.email.duplicateの追加
+			// エラー内容の保持
+			redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userForm", result);
+			// 入力値の保持
+			redirectAttributes.addFlashAttribute("userForm", form);
+			// 入力画面へ戻る
+			return "redirect:/client/user/regist/input";
 		}
 
-		// 正常時、セッション保存
+		// 正常時、確認画面にセッション保存
 		session.setAttribute("userForm", form);
-
+		// 確認画面へ
 		return "redirect:/client/user/regist/check";
 
 	}
 
 	/**
-	 * ④ 確認画面表示
+	 * ⑤ 確認画面表示
 	 */
 	@RequestMapping(path = "/check", method = RequestMethod.GET)
 	public String checkView(Model model) {
+		// セッションから入力フォーム情報を取得
 		UserForm form = (UserForm) session.getAttribute("userForm");
+		// 取得した情報を画面へ渡す
 		model.addAttribute("userForm", form);
+		// 確認画面の表示
 		return "client/user/regist_check";
 	}
-}
 
-//
-////	////入力画面（戻る対応）
-////	@RequestMapping(path = "/input", method = RequestMethod.POST)
-////	public String inputPost() {
-////		return "redirect:/client/user/regist/input";
-////	}
-//
-//}
-//]
-//		]
+	/**
+	 * ⑥ 登録処理（登録ボタン押した）
+	 */
+	// 画面で入力されたデータをDBに保存し完了画面へ
+	@RequestMapping(path = "/complete", method = RequestMethod.POST)
+	public String showUserRegistComplete() {
+		// セッションから入力されたフォーム情報を取得
+		UserForm form = (UserForm) session.getAttribute("userForm");
+		// Entity生成
+		User user = new User();
+		// FormからEntityを一括コピー （※後で手動setに変更
+		BeanUtils.copyProperties(form, user);
+		// DB登録
+		userRepository.save(user);
+		// セッション削除
+		session.removeAttribute("userForm");
+		// 完了画面へリダイレクト
+		return "redirect:/client/user/regist/complete";
+
+	}
+
+	/**
+	 * ⑦ 完了画面表示
+	 */
+	@RequestMapping(path = "/complete", method = RequestMethod.GET)
+	public String completeView() {
+
+		// 完了画面（regist_complete.html）を表示
+		return "client/user/regist_complete";
+	}
+
+}
