5af72dbdc397cb864c36511aca2068b2370eb520
nanba <nanba@NANBA-AEROG11.cac-k.jp.local>
2026-06-11 15:07:51 +0900

    update: 買い物かごの在庫確認の機能を修正

--- shared_shop/src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import jakarta.servlet.http.HttpSession;
 import jp.co.sss.shop.bean.BasketBean;
+import jp.co.sss.shop.entity.Item;
 import jp.co.sss.shop.repository.ItemRepository;
 
 /**
@@ -35,29 +36,76 @@ public class ClientBasketController {
 
 	/**
 	 * 買い物かご一覧表示処理
+	 * @param model Viewとの値受渡し
 	 * @return "client/basket/list" 買い物かご一覧表示
 	 * @return "redirect:/login" 未ログイン時にログイン画面へ
 	 */
 	@RequestMapping(path = "/client/basket/list", method = RequestMethod.GET)
-	public String showBasketList() {
+	public String showBasketList(Model model) {
 		// 会員情報の確認
 		if (session.getAttribute("user") == null) {
 			// 未ログインの場合、ログイン画面へ
 			return "redirect:/login";
 		}
+
+		// 買い物かごのリストを取得
+		List<BasketBean> basketBeans = (List<BasketBean>) session.getAttribute("basketBeans");
+
+		// エラーメッセージのリストを作成
+		List<String> itemNameListZero = new ArrayList<>();
+		List<String> itemNameListLessThan = new ArrayList<>();
+
+		// 買い物かごを確認
+		if (basketBeans != null) {
+			for (BasketBean basketBean : basketBeans) {
+				// 商品の情報を取得
+				Item item = itemRepository.getReferenceById(basketBean.getId());
+
+				// 商品情報を最新に更新
+				BeanUtils.copyProperties(item, basketBean, "orderNum");
+
+				// 在庫数を確認
+				if (item.getStock() <= 0) {
+					// 在庫数が0の場合のエラーメッセージの追加
+					itemNameListZero.add(basketBean.getName());
+					// 注文数を0に
+					basketBean.setOrderNum(0);
+				} else if (basketBean.getStock() < basketBean.getOrderNum()) {
+					// 在庫数 < 注文数 の場合のエラーメッセージの追加
+					itemNameListLessThan.add(basketBean.getName());
+					// 注文数を在庫数に合わせる
+					basketBean.setOrderNum(item.getStock());
+				}
+			}
+
+			// エラーメッセージをリクエストスコープ追加
+			model.addAttribute("itemNameListZero", itemNameListZero);
+			model.addAttribute("itemNameListLessThan", itemNameListLessThan);
+
+			// 在庫のない商品を買い物かごから削除
+			basketBeans.removeIf(basketBean -> basketBean.getStock() <= 0);
+
+			// セッションの買い物かごの情報を更新
+			if (basketBeans.size() == 0) {
+				// 買い物かごが空なら、セッションから買い物かご情報を削除
+				session.removeAttribute("basketBeans");
+			} else {
+				// セッション情報を最新に更新
+				session.setAttribute("basketBeans", basketBeans);
+			}
+		}
+
 		return "client/basket/list";
 	}
 
 	/**
 	 * idで主キー検索をし、リストに追加
 	 * @param id 追加したい商品のID
-	 * @param model Viewとの値受渡し
 	 * @return "redirect:/client/basket/list" 買い物かご一覧表示
-	 * @return "client/basket/list" エラーメッセージをスコープに追加し、買い物かご一覧を表示
 	 * @return "redirect:/login" 未ログイン時にログイン画面へ
 	 */
 	@RequestMapping(path = "/client/basket/add", method = RequestMethod.POST)
-	public String addBasket(Integer id, Model model) {
+	public String addBasket(Integer id) {
 		// 会員情報の確認
 		if (session.getAttribute("user") == null) {
 			// 未ログインの場合、ログイン画面へ
@@ -70,13 +118,12 @@ public class ClientBasketController {
 		BeanUtils.copyProperties(itemRepository.getReferenceById(id), inputbasketBean);
 
 		// 在庫数の確認
-		if (inputbasketBean.getStock() == 0) {
-			// エラーメッセージの追加
-			model.addAttribute("itemNameListZero", inputbasketBean.getName());
-			return "client/basket/list";
+		if (inputbasketBean.getStock() <= 0) {
+			// 在庫がないなら追加せずに、リダイレクト
+			return "redirect:/client/basket/list";
 		}
 
-		// セッションのリストを取得
+		// 買い物かごのリストを取得
 		List<BasketBean> basketBeans = (List<BasketBean>) session.getAttribute("basketBeans");
 
 		// リストが空なら作成する
@@ -87,16 +134,8 @@ public class ClientBasketController {
 			for (BasketBean bean : basketBeans) {
 				// 同じ商品があった場合
 				if (bean.getId().equals(id)) {
-					// 在庫数を確認
-					if (bean.getOrderNum().equals(bean.getStock())) {
-						// 在庫を上回った場合、エラーメッセージを追加
-						model.addAttribute("itemNameListLessThan", bean.getName());
-						return "client/basket/list";
-					} else {
-						// 在庫がある場合
-						// orderNumをインクリメントし、リダイレクト
-						bean.setOrderNum(bean.getOrderNum() + 1);
-					}
+					// orderNumをインクリメントし、リダイレクト
+					bean.setOrderNum(bean.getOrderNum() + 1);
 					return "redirect:/client/basket/list";
 				}
 			}
@@ -118,7 +157,7 @@ public class ClientBasketController {
 	 */
 	@RequestMapping(path = "/client/basket/delete", method = RequestMethod.POST)
 	public String deleteBasket(Integer id) {
-		// セッションのリストを取得
+		// 買い物かごのリストを取得
 		List<BasketBean> basketBeans = (List<BasketBean>) session.getAttribute("basketBeans");
 
 		// 対象の商品を探す
