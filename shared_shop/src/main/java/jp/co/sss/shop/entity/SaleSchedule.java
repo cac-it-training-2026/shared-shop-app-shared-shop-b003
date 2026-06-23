4e56382440b90c8cf978d9bc2559d11f4645a46b
nanba <nanba@NANBA-AEROG11.cac-k.jp.local>
2026-06-23 17:25:14 +0900

    fix: SaleScheduleのフィールドの修正

--- shared_shop/src/main/java/jp/co/sss/shop/entity/SaleSchedule.java
+++ shared_shop/src/main/java/jp/co/sss/shop/entity/SaleSchedule.java
@@ -1,5 +1,7 @@
 package jp.co.sss.shop.entity;
 
+import java.util.Date;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,90 +18,90 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "sale_schedule")
 public class SaleSchedule {
-    /**
-     * セールID
-     */
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_sale_schedule_gen")
-    @SequenceGenerator(name = "seq_sale_schedule_gen", sequenceName = "seq_sale_schedule", allocationSize = 1)
-    private Integer id;
+	/**
+	 * セールID
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_sale_schedule_gen")
+	@SequenceGenerator(name = "seq_sale_schedule_gen", sequenceName = "seq_sale_schedule", allocationSize = 1)
+	private Integer id;
 
-    /**
-     * カテゴリ
-     */
-    @ManyToOne
-    @JoinColumn(name = "category_id", referencedColumnName = "id")
-    private Category category;
+	/**
+	 * カテゴリ
+	 */
+	@ManyToOne
+	@JoinColumn(name = "category_id", referencedColumnName = "id")
+	private Category category;
 
-    /**
-     * 開始時間 (HH:mm:ss形式)
-     */
-    @Column(name = "start_time")
-    private String startTime;
+	/**
+	 * 開始時間 (HH:mm:ss形式)
+	 */
+	@Column(name = "start_time")
+	private Date startTime;
 
-    /**
-     * 終了時間 (HH:mm:ss形式)
-     */
-    @Column(name = "end_time")
-    private String endTime;
+	/**
+	 * 終了時間 (HH:mm:ss形式)
+	 */
+	@Column(name = "end_time")
+	private Date endTime;
 
-    /**
-     * 割引率
-     */
-    @Column(name = "discount_rate")
-    private Integer discountRate;
+	/**
+	 * 割引率
+	 */
+	@Column(name = "discount_rate")
+	private Integer discountRate;
 
-    /**
-     * 削除フラグ 0:未削除、1:削除済み
-     */
-    @Column(name = "delete_flag", insertable = false)
-    private Integer deleteFlag;
+	/**
+	 * 削除フラグ 0:未削除、1:削除済み
+	 */
+	@Column(name = "delete_flag", insertable = false)
+	private Integer deleteFlag;
 
-    public Integer getId() {
-        return id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public Category getCategory() {
-        return category;
-    }
+	public Category getCategory() {
+		return category;
+	}
 
-    public void setCategory(Category category) {
-        this.category = category;
-    }
+	public void setCategory(Category category) {
+		this.category = category;
+	}
 
-    public String getStartTime() {
-        return startTime;
-    }
+	public Date getStartTime() {
+		return startTime;
+	}
 
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
-    }
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
 
-    public String getEndTime() {
-        return endTime;
-    }
+	public Date getEndTime() {
+		return endTime;
+	}
 
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
 
-    public Integer getDiscountRate() {
-        return discountRate;
-    }
+	public Integer getDiscountRate() {
+		return discountRate;
+	}
 
-    public void setDiscountRate(Integer discountRate) {
-        this.discountRate = discountRate;
-    }
+	public void setDiscountRate(Integer discountRate) {
+		this.discountRate = discountRate;
+	}
 
-    public Integer getDeleteFlag() {
-        return deleteFlag;
-    }
+	public Integer getDeleteFlag() {
+		return deleteFlag;
+	}
 
-    public void setDeleteFlag(Integer deleteFlag) {
-        this.deleteFlag = deleteFlag;
-    }
+	public void setDeleteFlag(Integer deleteFlag) {
+		this.deleteFlag = deleteFlag;
+	}
 }
